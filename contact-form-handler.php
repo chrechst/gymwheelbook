99a1d30d6a7d0eca7aa853cc1feb920feb4ad37d
chrechst <67789318+chrechst@users.noreply.github.com>
2020-07-03 14:18:29 +0200

    Add files via upload

---
+++ contact-form-handler.php
@@ -0,0 +1,15 @@
+<?php
+
+if (isset($_POST['submit'])) {
+	$name = $_POST['name'];
+	$emailFrom = $_POST['email'];
+	$subject = $_POST['subject'];
+	$message = $_POST['message'];
+
+	$mailTo = "contact@gymwheelbook.com"
+	$headers = "From: ".$mailFrom;
+	$txt = "You have received an e-mail from ".$name.".\n\n".$message;
+
+	mail($mailTo, $subject, $txt, $headers);
+	header("Location: confirmation.html");
+}
